66680f768234b4ca3fdf48263328dcc0f2b82497
Felipe <acc.trash@keemail.me>
2024-03-01 20:11:06 -0300

    feat: First Commit

---
+++ src/main/java/br/com/univesp/pi/application/domain/model/Money.java
@@ -0,0 +1,14 @@
+package br.com.univesp.pi.application.domain.model;
+
+import lombok.Getter;
+import lombok.Setter;
+@Setter
+@Getter
+public class Money {
+
+    public Integer value;
+    public Integer decimals;
+    public String type;
+    public String signal;
+
+}
---
+++ src/main/java/br/com/univesp/pi/application/domain/model/Products.java
@@ -0,0 +1,12 @@
+package br.com.univesp.pi.application.domain.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Setter
+@Getter
+public class Products {
+    public List<Product> products;
+}
