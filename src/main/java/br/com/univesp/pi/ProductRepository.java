45cf9d8ebb4b7aa5abd8975e09bb78074067b662
Felipe <felipebelletato@hotmail.com>
2024-05-06 03:41:32 -0300

    fix: fixing regex at product retrival

--- src/main/java/br/com/univesp/pi/ProductRepository.java
+++ src/main/java/br/com/univesp/pi/ProductRepository.java
@@ -3,6 +3,7 @@ package br.com.univesp.pi;
 import br.com.univesp.pi.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -10,8 +11,7 @@ import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    //@Query("SELECT name FROM Product WHERE name LIKE '%?1%'")
-    @Query("SELECT id,name,description,price FROM Product WHERE name LIKE '%bolo%'")
-    public <T> ArrayList<T> getProductByRegex(String regex);
+    @Query("SELECT id,name,description,price FROM Product WHERE name LIKE CONCAT('%',:regex,'%')")
+    public <T> ArrayList<T> getProductByRegex(@Param("regex") String regex);
 
 }
