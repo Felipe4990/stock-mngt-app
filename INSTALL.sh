b19918e032755eb3466bcc500d0da1abebd7411f
Felipe <felipebelletato@hotmail.com>
2024-05-22 12:54:17 -0300

    feat(pipe): Adding support for creating html shortcut to Desktop

--- INSTALL.sh
+++ INSTALL.sh
@@ -3,6 +3,7 @@
 PUBLIC_PATH="/c/Users/Public"
 PROJECT_NAME=stock-mngt
 
+wmic process where "name like 'java.exe'" delete
 rm -rf $PUBLIC_PATH/$PROJECT_NAME
 mkdir -p $PUBLIC_PATH/$PROJECT_NAME
 curl https://download.java.net/openjdk/jdk21/ri/openjdk-21+35_windows-x64_bin.zip \
@@ -11,10 +12,11 @@ curl https://download.java.net/openjdk/jdk21/ri/openjdk-21+35_windows-x64_bin.zi
     -d $PUBLIC_PATH/$PROJECT_NAME \
 && rm -rf $PUBLIC_PATH/$PROJECT_NAME/tmp.zip
 JAVA_HOME="$PUBLIC_PATH/$PROJECT_NAME/jdk-21"; PATH=$PATH:$JAVA_HOME/bin; echo $PATH; ./gradlew clean build
-find "$(pwd)/build/libs" \
+find "$(pwd)/build/libs/controle-estoque-app" \
     -type f \
     -print0 \
 | xargs -0 mv -t $PUBLIC_PATH/$PROJECT_NAME
+mv $PUBLIC_PATH/$PROJECT_NAME/controle-estoque-app $PUBLIC_PATH/$PROJECT_NAME/controle-estoque-app.jar
 cp -r $(pwd)/src/main/resources $PUBLIC_PATH/$PROJECT_NAME
-cp -r front/ $PUBLIC_PATH/$PROJECT_NAME
+cp -r $(pwd)/front/ $PUBLIC_PATH/$PROJECT_NAME
 rm -rf build
--- RUN.sh
+++ RUN.sh
@@ -7,4 +7,8 @@ wmic process where "name like 'java.exe'" delete
 
 mkdir -p $PUBLIC_PATH/$PROJECT_NAME/log
 JAVA_HOME="$PUBLIC_PATH/$PROJECT_NAME/jdk-21"; PATH=$PATH:$JAVA_HOME/bin; echo $PATH; nohup java \
-    -jar $PUBLIC_PATH/$PROJECT_NAME/controle-estoque-app-1.0.0.jar --spring.config.location=$PUBLIC_PATH/$PROJECT_NAME/resources/application.yaml > $PUBLIC_PATH/$PROJECT_NAME/log/output.log 2>&1 &
+    -jar $PUBLIC_PATH/$PROJECT_NAME/controle-estoque-app.jar --spring.config.location=$PUBLIC_PATH/$PROJECT_NAME/resources/application.yaml > $PUBLIC_PATH/$PROJECT_NAME/log/output.log 2>&1 &
+
+cp $(pwd)/assets/create-shortcut.bat $PUBLIC_PATH/$PROJECT_NAME
+chmod +x $PUBLIC_PATH/$PROJECT_NAME/create-shortcut.bat
+$PUBLIC_PATH/$PROJECT_NAME/create-shortcut.bat
