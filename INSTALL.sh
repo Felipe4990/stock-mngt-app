408c628780397f4f77d70e070842a6fc29d623b1
Felipe <felipebelletato@hotmail.com>
2024-05-21 22:01:17 -0300

    feat(pipe): Adding install script

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
+PUBLIC_PATH="/c/Users/Public"
+PROJECT_NAME=stock-mngt
 
-
-#mkdir tmp; cd tmp
-#echo $(pwd)
-#curl https://download.java.net/openjdk/jdk21/ri/openjdk-21+35_windows-x64_bin.zip -o /tmp/tmp.zip && unzip /tmp/tmp.zip && rm /tmp/tmp.zip
-$(pwd)/tmp/jdk-21/bin/java.exe -version
-
-$(pwd)/tmp/jdk-21/bin/java.exe -jar %home%/controle-estoque-app-0.0.1-SNAPSHOT.jar --spring.config.location=src/main/java/resources/application.yaml
-
+rm -rf $PUBLIC_PATH/$PROJECT_NAME
+mkdir -p $PUBLIC_PATH/$PROJECT_NAME
+curl https://download.java.net/openjdk/jdk21/ri/openjdk-21+35_windows-x64_bin.zip \
+    -o $PUBLIC_PATH/$PROJECT_NAME/tmp.zip \
+&& unzip $PUBLIC_PATH/$PROJECT_NAME/tmp.zip \
+    -d $PUBLIC_PATH/$PROJECT_NAME \
+&& rm -rf $PUBLIC_PATH/$PROJECT_NAME/tmp.zip
+JAVA_HOME="$PUBLIC_PATH/$PROJECT_NAME/jdk-21"; PATH=$PATH:$JAVA_HOME/bin; echo $PATH; ./gradlew clean build
+find "$(pwd)/build/libs" \
+    -type f \
+    -print0 \
+| xargs -0 mv -t $PUBLIC_PATH/$PROJECT_NAME
+cp -r $(pwd)/src/main/resources $PUBLIC_PATH/$PROJECT_NAME
+cp -r front/ $PUBLIC_PATH/$PROJECT_NAME
+rm -rf build
